3c86008b3cf1efa669881f44eb827714f0e5495e
Carl Aiau <hello@carlaiau.com>
2026-05-19 15:55:15 +1200

    home mobile nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,6 +121,7 @@ function MobileNavigation(
         </div>
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
+            <MobileNavItem href="/">Home</MobileNavItem>
             <MobileNavItem href="/about">About</MobileNavItem>
           </ul>
         </nav>
